a8daa941b176395204961f28bab010ce0307be49
tysonstolk-create <tyson.stolk@gmail.com>
2026-05-29 18:10:58 -0600

    feat: iPhone PWA with Jersey Mike's logo splash and BOGO coupon screen

--- app/sw.js
+++ app/sw.js
@@ -1,20 +1,16 @@
-const CACHE = 'jm-coupon-v3';
-const ASSETS = ['./', './index.html', './manifest.json', './bg.png'];
+const CACHE = 'jm-coupon-v2';
+const ASSETS = ['./index.html', './manifest.json', './bg.png', './coupon.png', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS.filter(a => a !== './coupon.png'))).then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(() => {})));
 });
